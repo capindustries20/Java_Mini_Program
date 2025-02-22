e1e6ec6b5dbd11010950db858f11d0231720e310
capindustries20 <capindustries82524@gmail.com>
2025-02-22 17:37:36 +0530

    Java Mini Program

---
+++ src/main/java/org/example/Palindrome.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Palindrome {
+    public static boolean isPalindrome(int num){
+        int original = num, rev =0;
+
+        while (num > 0){
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        return original == rev;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121) ? "Yes" : "No");
+    }
+
+}
